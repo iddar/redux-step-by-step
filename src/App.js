65217f92d8d146e774cc75806b75d43bbeb43c1f
iddar <iddar@dbug.mx>
2018-01-16 10:45:41 -0600

    include simple way

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import { createStore } from 'redux'
+
+import logo from './logo.svg'
+import './App.css'
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text])
+    default:
+      return state
+  }
+}
+
+const store = createStore(todos)
 
 class App extends Component {
+  componentWillMount () {
+    store.subscribe(() => this.forceUpdate())
+  }
+
   render() {
+    console.warn(store.getState())
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" onClick={() => {
+              store.dispatch({
+                type: 'ADD_TODO',
+                text: 'Read the docs'
+              })
+            }} />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+    )
   }
 }
 
